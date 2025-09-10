51a940f5bbc9eae710adee8455385721593ee530
Himanshu Chand <hchand@navigate360.com>
2025-09-10 17:26:19 +0530

    updated the code base to remove the CVEs found in npm audit

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,20 @@
-const path = require('path')
-const SRC_PATH = path.join(__dirname, '../src')
+const path = require('path');
+const SRC_PATH = path.join(__dirname, '../src');
 
 module.exports = ({ config }) => {
-	config.module.rules.push({
-		test: /\.(ts|tsx)$/,
-		include: [SRC_PATH],
-		use: [
-			{
-				loader: require.resolve('awesome-typescript-loader'),
-				options: {
-					configFileName: './tsconfig.json'
-				}
-			}
-		]
-	})
-	config.resolve.extensions.push('.ts', '.tsx')
-	return config
-}
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: [SRC_PATH],
+    use: [
+      {
+        loader: require.resolve('ts-loader'),
+        options: {
+          transpileOnly: true, // faster builds, type checking handled by tsc separately
+        },
+      },
+    ],
+  });
+
+  config.resolve.extensions.push('.ts', '.tsx');
+  return config;
+};
